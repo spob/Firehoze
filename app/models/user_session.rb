4ede72d81f5dcad33a946d3cbe35ae57a2bedbbb
Bob Sturim <bob@sturim.org>
2009-04-16 21:51:13 -0400

    Added settings to disable user after multiple failed logins

--- app/models/user_session.rb
+++ app/models/user_session.rb
@@ -1,4 +1,9 @@
 class UserSession < Authlogic::Session::Base
   # Turn on the option to log the user out after inactivity
   logout_on_timeout true
+
+  # disable the account after 10 failed login attempts
+  consecutive_failed_logins_limit 10
+  # and disable it for 1 hour
+  failed_login_ban_for 1.hour
 end
