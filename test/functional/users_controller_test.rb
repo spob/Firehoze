0407feb9eb4a502c7bdcd5cf296095f882a3c9a0
Bob Sturim <bob@sturim.org>
2009-04-21 22:51:11 -0400

    Got functional tests working

--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -1,34 +1,59 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-#  test "should get new" do
-#    get :new
-#    assert_response :success
-#  end
-#
-#  test "should create user" do
-#    assert_difference('User.count') do
-#      post :create, :user => { :login => "ben", :password => "benrocks", :password_confirmation => "benrocks", :email => "myemail@email.com" }
-#    end
-#
-#    assert_redirected_to account_path
-#  end
-#
-#  test "should show user" do
-#    set_session_for(users(:ben))
-#    get :show
-#    assert_response :success
-#  end
-#
-#  test "should get edit" do
-#    set_session_for(users(:ben))
-#    get :edit, :id => users(:ben).id
-#    assert_response :success
-#  end
-#
-#  test "should update user" do
-#    set_session_for(users(:ben))
-#    put :update, :id => users(:ben).id, :user => { }
-#    assert_redirected_to account_path
-#  end
+
+  context "while not logged on" do
+
+    context "on GET to :new" do
+      setup { get :new }
+
+      should_assign_to :user
+      should_respond_with :success
+      should_not_set_the_flash
+      should_render_template "new"
+    end
+
+    context "on POST to :create" do
+      setup { post :create, :user => Factory.attributes_for(:user) }
+
+      should_assign_to :user
+      should_respond_with :redirect
+      should_set_the_flash_to "Account registered!"
+      should_redirect_to("account page") { account_url }
+    end
+  end
+
+  context "when logged on" do
+    setup do
+      activate_authlogic
+      UserSession.create Factory(:user)
+    end
+
+    context "on GET to :show" do
+      setup { get :show, :user => Factory(:user) }
+
+      should_assign_to :user
+      should_respond_with :success
+      should_not_set_the_flash
+      should_render_template "show"
+    end
+
+    context "on GET to :edit" do
+      setup { get :edit, :user => Factory(:user) }
+
+      should_assign_to :user
+      should_respond_with :success
+      should_not_set_the_flash
+      should_render_template "edit"
+    end
+
+    context "on PUT to :update" do
+      setup { put :update, :id => Factory(:user).id, :user => Factory.attributes_for(:user) }
+
+      should_assign_to :user
+      should_respond_with :redirect
+      should_set_the_flash_to "Account updated!"
+      should_redirect_to("account page") { account_url }
+    end
+  end
 end
