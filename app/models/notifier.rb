697d9ce47b18454bce3426e25200fc99c2266a0b
Bob Sturim <bob@sturim.org>
2009-04-21 21:20:49 -0400

    Moved email settings to the configuration file

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -1,9 +1,13 @@
 class Notifier < ActionMailer::Base
-  default_url_options[:host] = "www.braindump.com"
+  default_url_options.update :protocol => APP_CONFIG['protocol']
+  default_url_options.update :host => APP_CONFIG['host']
+  default_url_options.update :port => APP_CONFIG['port']
+
+#  default_url_options[:host] = "www.braindump.com"
 
   def password_reset_instructions(user)
     subject     "Your password has been reset"
-    from        "admin@braindump.com"
+    from         APP_CONFIG['admin_email']
     recipients  user.email
     sent_on     Time.zone.now
     body        :edit_password_reset_url => edit_password_reset_url(user.perishable_token)
