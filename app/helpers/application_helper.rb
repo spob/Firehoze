c6e2d084ec797b561e8c8bf1ef186fb4ed6fb868
Bob Sturim <bob@sturim.org>
2009-04-28 22:49:21 -0400

    Added todo comment for i18n

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,12 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
   # Format a date and adjust the timezone for the user's timezone
+  # TODO: make i18n compatible
   def format_date_time(the_date)
     the_date.strftime("%b %d, %Y %I:%M%p") if the_date
   end
 
+  # TODO: make i18n compatible
   def format_date(the_date)
     the_date.strftime("%b %d, %Y") if the_date
   end
