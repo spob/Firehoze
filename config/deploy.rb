fe213d9fd0382eff2aac23ff6ba0284ab12beb8b
Bob Sturim <bob@sturim.org>
2009-06-05 17:45:22 -0400

    Several minor tweaks to the capistrano deploy task, including most significantly preserving the assets directory across deployments

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,11 @@
 set :application, 'Firehoze'
 set :user, 'firehoz'
 set :domain, '68.233.8.4'
+set :rails_env, 'production'
 set :mongrel_port, '4098'
-set :server_hostname, '68.233.8.4'
+set :server_hostname, domain
 set :keep_releases, 4
+set :runner, user
 
 set :git_account, 'spob'
 set :scm_passphrase,  Proc.new { Capistrano::CLI.password_prompt('F1reh0ze') }
@@ -14,8 +16,8 @@ role :db, server_hostname, :primary => true
 
 default_run_options[:pty] = true
 set :repository,  "git@github.com:spob/Firehoze.git"
-set :scm, "git"
-set :user, user
+set :scm, :git
+#set :user, user
 
 ssh_options[:forward_agent] = true
 set :branch, "master"
@@ -31,6 +33,8 @@ namespace :deploy do
   task :finishing_touches, :roles => :app do
     run "cp -pf #{deploy_to}/to_copy/production.rb #{current_path}/config/environments/production.rb"
     run "cp -pf #{deploy_to}/to_copy/database.yml #{current_path}/config/database.yml"
+    # preserve the assets directory which resides under shared
+    run "ln -s #{shared_path}/assets #{release_path}/public/assets" 
   end
 
   deploy.task :restart, :roles => :app do
@@ -70,5 +74,12 @@ namespace :task_server do
   desc "Stop Task Server"
   task :stop, :roles => :app do
     run "ruby #{current_path}/script/task_server_control.rb stop -- -e production"
+  end
+end
+
+namespace :gems do
+  desc "Show installed gems"
+  task :list do
+    stream "gem list"
   end
 end
