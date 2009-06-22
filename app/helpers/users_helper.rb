36e6dfc4d27995ef9e89e440c1006c02280a9e23
rsturim <rich@sturim.org>
2009-06-22 00:52:05 -0400

    adding blueprintCSS framework

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,15 @@
 module UsersHelper
+  def signin_or_signout_link
+    if current_user
+      "Greetings, #{current_user.login} (not #{signout_link(current_user.login)}?) #{signout_link("Sign Out")}"
+    else
+      link_to "Sign In", login_path
+    end
+  end
+
+
+  def signout_link(string)
+    link_to "#{string}", logout_path, :method => :delete
+  end
+
 end
