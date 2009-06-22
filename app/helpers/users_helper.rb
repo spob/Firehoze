d2530ec7d6499468c2c4762ea1e418c9abc0f035
rsturim <rich@sturim.org>
2009-06-22 01:16:28 -0400

    add register link to top nav

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,13 +3,11 @@ module UsersHelper
     if current_user
       "Greetings, #{current_user.login} (not #{signout_link(current_user.login)}?) #{signout_link("Sign Out")}"
     else
-      link_to "Sign In", login_path
+      "#{link_to "Sign In", login_path} | #{link_to "Register", new_registration_path}"
     end
   end
-
 
   def signout_link(string)
     link_to "#{string}", logout_path, :method => :delete
   end
-
 end
