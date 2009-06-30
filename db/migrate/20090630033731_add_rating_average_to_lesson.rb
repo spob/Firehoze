3d59f5f43cd5781ea1b22e03fb8cff902e2deb63
rsturim <rich@sturim.org>
2009-06-30 00:54:47 -0400

    dang, without the correct scale/precision -- the stars are garbage

--- db/migrate/20090630033731_add_rating_average_to_lesson.rb
+++ db/migrate/20090630033731_add_rating_average_to_lesson.rb
@@ -1,6 +1,6 @@
 class AddRatingAverageToLesson < ActiveRecord::Migration
   def self.up
-    add_column :lessons, :rating_average, :decimal, :default => 0
+    add_column :lessons, :rating_average, :decimal, :default => 0, :precision => 3, :scale => 2
   end
 
   def self.down
