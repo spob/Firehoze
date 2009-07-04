9644be2e5d61f3a3674de7ddad6b450761125666
rsturim <rich@sturim.org>
2009-07-03 22:41:02 -0400

    addresses bug 'User shouldn't be able to rate a lesson he has not purchased'

--- app/helpers/lessons_helper.rb
+++ app/helpers/lessons_helper.rb
@@ -3,9 +3,14 @@ module LessonsHelper
     if current_user.nil? or current_user.owns_lesson? lesson or current_user == lesson.instructor
       "Watch Lesson"
     elsif lesson.free_credits.available.size > 0
-       "Watch For Free" 
+      "Watch For Free" 
     else
       "Acquire Lesson"
     end
   end
+
+  def lesson_rating_for(lesson, user)
+    ratings_for(lesson, (user != @lesson.instructor and user.owns_lesson?(lesson)) ? :active : :static)
+  end
+
 end
