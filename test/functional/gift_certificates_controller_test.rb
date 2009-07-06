cb6ad53f64384a37233796aaa198c2984966aed5
Bob Sturim <bob@sturim.org>
2009-07-05 23:17:12 -0400

    Added unit test for redeem controller method

--- test/functional/gift_certificates_controller_test.rb
+++ test/functional/gift_certificates_controller_test.rb
@@ -1,8 +1,27 @@
 require 'test_helper'
 
-class GiftCertificateControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+class GiftCertificatesControllerTest < ActionController::TestCase
+
+  context "when logged on" do
+    setup do
+      activate_authlogic
+      @user = Factory(:user)
+      UserSession.create @user
+    end
+
+    context "with a gift_certificate" do
+      setup { @gift_certificate = Factory.create(:gift_certificate) }
+      
+      context "on POST to :redeem" do
+        setup do
+          post :redeem, :id => @gift_certificate
+        end
+
+        should_assign_to :gift_certificate
+        should_respond_with :redirect
+        should_set_the_flash_to /redeemed/
+        should_redirect_to("account page") { account_path }
+      end
+    end
   end
 end
