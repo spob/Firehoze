2f97eac072c976e532634fe2d046e1b115088f97
rsturim <rich@sturim.org>
2009-07-07 22:56:34 -0400

    going back to the original way, calling it a night

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,33 +2,3 @@
 // This file is automatically included by javascript_include_tag :defaults
 
 // public/javascripts/application.js
-
-
-
-jQuery.ajaxSetup({ 'beforeSend': function(xhr) {xhr.setRequestHeader("Accept", "text/javascript")} })
- // 
- // function _ajax_request(url, data, callback, type, method) {
- //     if (jQuery.isFunction(data)) {
- //         callback = data;
- //         data = {};
- //     }
- //     return jQuery.ajax({
- //         type: method,
- //         url: url,
- //         data: data,
- //         success: callback,
- //         dataType: type
- //         });
- // }
- //  
- // jQuery.extend({
- //     put: function(url, data, callback, type) {
- //         return _ajax_request(url, data, callback, type, 'PUT');
- //     },
- //     delete_: function(url, data, callback, type) {
- //         return _ajax_request(url, data, callback, type, 'DELETE');
- //     }
- // });
- //  
- // 
- 
