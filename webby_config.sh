1f6991643ea521808f929a5f5859eb9313d7271c
Bob Sturim <bob@sturim.org>
2009-07-08 22:40:30 -0400

    More tweaks for webbynode

--- webby_config.sh
+++ webby_config.sh
@@ -3,8 +3,8 @@ echo "gem: --no-ri --no-rdoc" >> ~/.gemrc
 
 # install required gems
 sudo gem install hpricot
+sudo gem install right_http_connection
 
-sudo rake gems:install
 
 # Assembles Rails database.yml based on information
 # provided from a ReadyStack redeploy
@@ -16,18 +16,18 @@ echo WC_DB_ENGINE=${WC_DB_ENGINE}
 
 echo "
 login: &login
-adapter: ${WC_DB_ENGINE}
-database: ${WC_APP_NAME}
-username: ${WC_APP_NAME}
-password: ${WC_DB_PASSWORD}
-host: localhost
+  adapter: ${WC_DB_ENGINE}
+  database: ${WC_APP_NAME}
+  username: ${WC_APP_NAME}
+  password: ${WC_DB_PASSWORD}
+  host: localhost
 " > config/database.yml
 
 if [ "${WC_DB_ENGINE}" == "mysql" ]; then
 echo "
 production:
-<<: *login
-encoding: utf8
+  <<: *login
+  encoding: utf8
 " >> config/database.yml
 fi
 
@@ -43,3 +43,6 @@ fi
 
 mkdir log
 chown www-data log
+
+rake db:migrate RAILS_ENV=production
+rake db:seed RAILS_ENV=production
