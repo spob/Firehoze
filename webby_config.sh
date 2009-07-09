d9b4759e1398653744f4b273599f1c331538a717
Bob Sturim <bob@sturim.org>
2009-07-08 23:39:02 -0400

    More work on deployment configuration

--- webby_config.sh
+++ webby_config.sh
@@ -47,3 +47,18 @@ chown www-data log
 
 rake db:migrate RAILS_ENV=production
 rake db:seed RAILS_ENV=production
+
+sudo apt-get install monit
+
+sudo sed -i "s/# set daemon  120/set daemon  60/" /etc/monit/monitrc
+
+sudo echo "
+#--------------task server
+check process taskserver with pidfile /var/rails/Firehoze/tmp/pids/task_server.rb.pid
+  start program = \"/usr/bin/ruby  /var/rails/Firehoze/script/task_server_control.rb start -f -- -e production\"
+  stop program = \"/usr/bin/ruby /var/rails/Firehoze/script/task_server_control.rb stop -- -e production\"
+  group taskserver
+" >> /etc/monit/monitrc
+
+sudo sed -i "s/startup=0/startup=1/" /etc/default/monit
+sudo monit -c /etc/monit/monitrc
