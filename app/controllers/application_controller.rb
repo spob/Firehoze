c5f02cef027fba020a1de8608f6031eb9a2a4cc2
rsturim <rich@sturim.org>
2009-07-10 22:12:24 -0400

    remove deprecation warning

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
   before_filter :set_timezone
   before_filter :set_user_language
 
-  protect_from_forgery :secret => "YlbDGsiBWemGmMMRpVO4ZaXegEAG0UcwkkyMD1ql"
+  protect_from_forgery :only => [:update, :delete, :create]
 
   def available_locales; AVAILABLE_LOCALES; end
 
