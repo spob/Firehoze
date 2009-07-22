865be975713b165ebe4de0ef8bd990b4587af4ac
Bob Sturim <bob@sturim.org>
2009-07-22 16:02:08 -0400

    Added an index to lesson_visits

--- db/migrate/20090722193530_add_purchased_to_lesson_visit.rb
+++ db/migrate/20090722193530_add_purchased_to_lesson_visit.rb
@@ -3,9 +3,13 @@ class AddPurchasedToLessonVisit < ActiveRecord::Migration
     add_column :lesson_visits, :owned, :boolean, :null => false, :default => false
     add_column :lesson_visits, :purchased_this_visit, :boolean, :null => false, :default => false
     add_column :lesson_visits, :rolled_up_at, :datetime
+
+    add_index :lesson_visits, :rolled_up_at
   end
 
   def self.down
+    remove_index :lesson_visits, :rolled_up_at
+    
     remove_column :lesson_visits, :owned
     remove_column :lesson_visits, :purchased_this_visit
     remove_column :lesson_visits, :rolled_up_at
