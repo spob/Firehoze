b89039079b4813269ab16b00c3755faa3565cd2d
Bob Sturim <bob@sturim.org>
2009-07-23 16:49:40 -0400

    Add buying patterns rollup job

--- app/models/lesson_buy_pattern.rb
+++ app/models/lesson_buy_pattern.rb
@@ -3,11 +3,30 @@ class LessonBuyPattern < ActiveRecord::Base
   belongs_to :purchased_lesson, :class_name => 'Lesson'
   validates_presence_of :lesson, :purchased_lesson, :counter
 
-  def self.compile_buy_patterns
+  named_scope :by_lesson,
+              lambda{|lesson_id|{:conditions => { :lesson_id => lesson_id }}
+              }
+  named_scope :by_purchased_lesson,
+              lambda{|purchased_lesson_id|{:conditions => { :purchased_lesson_id => purchased_lesson_id }}
+              }
+
+  def self.rollup_buy_patterns
     # First find recent (uncompiled) purchases
-    for visit in LessonVisit.uncompiled_lesson_purchases
-      for other_visit in LessonVisit.by_session_id(visit.session_id)
-        # and loop through all browsed video done on the same session
+    LessonBuyPattern.transaction do
+      for visit in LessonVisit.uncompiled_lesson_purchases
+        puts "here for #{visit.id}"
+        visit.update_attribute(:rolled_up_at, Time.zone.now)
+        for other_visit in LessonVisit.by_session(visit.session_id)
+          buy_pattern = LessonBuyPattern.by_purchased_lesson(visit.lesson.id).by_lesson(other_visit.lesson.id).first
+          if buy_pattern
+            puts "Found #{buy_pattern.id} with counter #{buy_pattern.counter}"
+            buy_pattern.update_attributes!(:counter => buy_pattern.counter + 1)
+          else
+            LessonBuyPattern.create!(:purchased_lesson => visit.lesson,
+                                     :lesson => other_visit.lesson,
+                                     :counter => 1)
+          end
+        end
       end
     end
   end
