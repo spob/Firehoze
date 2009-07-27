0d823627838066fce2a6a202a0004e67be0d68f1
Bob Sturim <bob@sturim.org>
2009-07-26 21:49:48 -0400

    Added unit tests for wish list

--- test/functional/wish_lists_controller_test.rb
+++ test/functional/wish_lists_controller_test.rb
@@ -1,8 +1,113 @@
 require 'test_helper'
 
 class WishListsControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  context "with an existing lesson" do
+    setup { @lesson = Factory.create(:lesson) }
+
+    context "and when logged on" do
+      setup do
+        activate_authlogic
+        @user = Factory(:user)
+        UserSession.create(@user)
+      end
+
+      context "on POST to :create" do
+        setup do
+          post :create, :id => @lesson
+        end
+
+        should_assign_to :lesson
+        should_respond_with :redirect
+        should_set_the_flash_to "Lesson added to your wish list"
+        should_redirect_to("show lesson page") { lesson_url(@lesson) }
+
+        should "wishes for the lesson" do
+          assert @user.on_wish_list?(@lesson)
+        end
+      end
+
+      context "on POST to :create when already owned" do
+        setup do
+          @user.credits.create!(:price => 0.99, :lesson => @lesson, :acquired_at => Time.now,
+                                :line_item => Factory.create(:line_item))
+          assert @user.owns_lesson?(@lesson)
+          post :create, :id => @lesson
+        end
+
+        should_assign_to :lesson
+        should_respond_with :redirect
+        should_set_the_flash_to /You already own/
+        should_redirect_to("show lesson page") { lesson_url(@lesson) }
+
+        should "does not wish for the lesson" do
+          assert !@user.on_wish_list?(@lesson)
+        end
+      end
+
+      context "on POST to :create when you are the author" do
+        setup do
+          @lesson2 = Factory.create(:lesson, :instructor => @user)
+          assert @lesson2.instructor == @user
+          post :create, :id => @lesson2
+        end
+
+        should_assign_to :lesson
+        should_respond_with :redirect
+        should_set_the_flash_to /As the author of this video/
+        should_redirect_to("show lesson page") { lesson_url(@lesson2) }
+
+        should "does not wish for the lesson" do
+          assert !@user.on_wish_list?(@lesson2)
+        end
+      end
+
+      context "on POST to :create when already on the wish list" do
+        setup do
+          @user.wishes << @lesson
+          assert @user.on_wish_list?(@lesson)
+          post :create, :id => @lesson
+        end
+
+        should_assign_to :lesson
+        should_respond_with :redirect
+        should_set_the_flash_to /lesson is already on your wish list/
+        should_redirect_to("show lesson page") { lesson_url(@lesson) }
+
+        should "does not wish for the lesson" do
+          assert @user.on_wish_list?(@lesson)
+        end
+      end
+
+      context "on DELETE to :destroy" do
+        setup do
+          @user.wishes << @lesson
+          assert @user.on_wish_list?(@lesson)
+          delete :destroy, :id => @lesson
+        end
+
+        should_set_the_flash_to /Lesson has been removed/
+        should_respond_with :redirect
+        should_redirect_to("show lesson page") { lesson_url(@lesson) }
+
+        should "not wish for the lesson" do
+          assert !@user.on_wish_list?(@lesson)
+        end
+      end
+
+      context "on DELETE to :destroy when not on the wait list already" do
+        setup do
+          assert !@user.on_wish_list?(@lesson)
+          delete :destroy, :id => @lesson
+        end
+
+        should_set_the_flash_to "This lesson is not on your wishlist"
+        should_respond_with :redirect
+        should_redirect_to("show lesson page") { lesson_url(@lesson) }
+
+        should "not wish for the lesson" do
+          assert !@user.on_wish_list?(@lesson)
+        end
+      end
+    end
   end
 end
