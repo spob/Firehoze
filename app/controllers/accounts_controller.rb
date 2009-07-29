f8d6eef7d02409d8502ba2733583b8db4c3a7d3f
Rich Sturim <richsturim@rich-sturims-macbookpro.local>
2009-07-29 09:41:34 -0400

    BREAKING COMMIT -- getting user avatars setup ... Bob, I need help with this. For some reason I cannot get the 'accounts routing' to work as expected -- I have to go, will be back later

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -13,19 +13,30 @@ class AccountsController < ApplicationController
   end
 
   def update
-    # avoid mass assignment here
+
     @user = @current_user
-    @user.email = params[:user][:email].try(:strip)
-    @user.first_name = params[:user][:first_name].try(:strip)
-    @user.last_name = params[:user][:last_name].try(:strip)
-    @user.bio = params[:user][:bio]
-    @user.time_zone = params[:user][:time_zone]
-    @user.language = params[:user][:language]
+
+    # avoid mass assignment here
+    if params[:user][:destroy_avatar] == 'true'
+      @user.avatar.clear
+    else
+      @user.email = params[:user][:email].try(:strip)
+      @user.first_name = params[:user][:first_name].try(:strip)
+      @user.last_name = params[:user][:last_name].try(:strip)
+      @user.bio = params[:user][:bio]
+      @user.time_zone = params[:user][:time_zone]
+      @user.language = params[:user][:language]
+      @user.avatar = params[:user][:avatar] if !params[:user][:avatar].blank?
+    end
+
     if @user.save
       flash[:notice] = t 'profile.update_success'
       redirect_to edit_user_path(@user)
-    else
-      render :action => :edit
     end
+
+  rescue Exception => e
+    raise params.inspect
+    flash[:error] = e.message
+    render edit_user_path(@user)
   end
 end
