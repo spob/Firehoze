6a13509734c5974fc3180654ae8a181acfde5be3
Rich Sturim <richsturim@rich-sturims-macbookpro.local>
2009-08-07 01:41:46 -0400

    mega commit surrounding form formatting

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,7 +3,7 @@ module UsersHelper
     if current_user
       "Greetings, #{current_user.login} (not #{signout_link(current_user.login)}?) #{signout_link("Sign Out")}"
     else
-      "#{link_to "Sign In", login_path} | #{link_to "Register", new_registration_path}"
+      "#{link_to "Sign In", login_path} | #{link_to "Sign Up", new_registration_path}"
     end
   end
 
