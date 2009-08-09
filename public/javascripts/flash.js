605250fa713a7bee92c27dee23ec13a2fbf2185d
Rich Sturim <richsturim@rich-sturims-macbookpro.local>
2009-08-09 11:52:57 -0400

    adjust flash

--- public/javascripts/flash.js
+++ public/javascripts/flash.js
@@ -3,7 +3,7 @@ $(function(){
         $(".flash").slideDown(400);
     }
     setTimeout(function(){
-        $(".flash").slideUp(750, function () {
+        $(".flash").fadeOut(750, function () {
             $(".flash").remove();
         });
     }, 3000);
