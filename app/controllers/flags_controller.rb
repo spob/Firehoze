8d22622370eb4c96ba76fd2e8321c7b292b8dc55
Bob Sturim <bob@sturim.org>
2009-08-09 22:50:48 -0400

    Working on flagging a lesson

--- app/controllers/flags_controller.rb
+++ app/controllers/flags_controller.rb
@@ -1,24 +1,30 @@
 class FlagsController < ApplicationController
-  before_filter :find_flagger, :require_user
+  before_filter :require_user
+  before_filter :find_flagger, :except => [ :new ]
 
   verify :method => :post, :only => [:create ], :redirect_to => :home_path
-  
+
   def create
     flag = @flagger.flags.create(params[:flags])
     respond_to do |format|
       format.html {redirect_to :controller => @flagger.class.to_s.pluralize.downcase, :action => :show, :id => @flagger.id}
-    end    
+    end
   end
 
   def new
-    
+    new_flag
   end
 
   private
 
   def find_flagger
     @klass = params[:flagger_type].capitalize.constantize
-    @flagger = klass.find(params[:flagger_id])
+    @flagger = @klass.find(params[:flagger_id])
+  end
+
+  def new_flag
+    @klass = params[:flagger_type].capitalize.constantize
+    @flag = @klass.flags.new
   end
 
 end
