d40afd9b6269bfa1bbcf51d2c3412dd72672a600
Bob Sturim <bob@sturim.org>
2009-08-10 21:44:04 -0400

    Got basic unit tests working

--- test/functional/flags_controller_test.rb
+++ test/functional/flags_controller_test.rb
@@ -1,8 +1,39 @@
 require 'test_helper'
 
 class FlagsControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  context "when logged on" do
+    setup do
+      activate_authlogic
+      @user = Factory(:user)
+      UserSession.create @user
+    end
+
+    context "given an existing lesson" do
+      setup do
+        @lesson = Factory.create(:lesson)
+      end
+
+      context "on GET to :new" do
+        setup { get :new, :flagger_type => 'Lesson', :flagger_id => @lesson }
+
+        should_assign_to :flag
+        should_assign_to :flagger
+        should_respond_with :success
+        should_not_set_the_flash
+        should_render_template "new"
+      end
+
+      context "on POST to :create" do
+        setup do
+          @new_flag_attr = Factory.attributes_for(:flag, :flaggable_id => @lesson.id)
+          post :create, :flag => @new_flag_attr, :flagger_type => 'Lesson', :flagger_id => @lesson
+        end
+
+        should_assign_to :flagger
+        should_respond_with :flag
+        should_set_the_flash_to /Thank you for bringing this to our attention/
+        should_redirect_to("Show lessons page") { lesson_url(@lesson) }
+      end
+    end
   end
 end
