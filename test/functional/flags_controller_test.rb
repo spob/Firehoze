4aa05dbe023830c80a6082c20fd371e52c9443c9
Bob Sturim <bob@sturim.org>
2009-08-11 22:30:41 -0400

    added unit tests for flagging reviews...another story bites the dust. Booyah!

--- test/functional/flags_controller_test.rb
+++ test/functional/flags_controller_test.rb
@@ -45,5 +45,47 @@ class FlagsControllerTest < ActionController::TestCase
         end
       end
     end
+
+    context "given an existing review" do
+      setup do
+        @credit = Factory.create(:credit, :user => @user)
+        @review = @credit.lesson.reviews.create!(:body => 'hello',
+                                                 :headline => 'headline',
+                                                 :user => @user)
+        assert @credit.lesson.reviewed_by?(@user)
+      end
+
+      context "on GET to :new" do
+        setup { get :new, :flagger_type => 'Review', :flagger_id => @review }
+
+        should_assign_to :flag
+        should_assign_to :flagger
+        should_respond_with :success
+        should_not_set_the_flash
+        should_render_template "new"
+      end
+
+      context "on POST to :create" do
+        setup do
+          @new_flag_attr = Factory.attributes_for(:flag, :flaggable_id => @review.id)
+          post :create, :flag => @new_flag_attr, :flagger_type => 'Review', :flagger_id => @review
+        end
+
+        should_assign_to :flagger
+        should_respond_with :redirect
+        should_set_the_flash_to /Thank you for bringing this to our attention/
+        #should_redirect_to("Show lessons page") { '"url_for(:controller => 'Lessons', :action => 'show', :id => id)"' }
+
+        context "and try to flag a second time" do
+          setup do
+            post :create, :flag => @new_flag_attr, :flagger_type => 'Review', :flagger_id => @review
+          end
+
+          should_assign_to :flagger
+          should_respond_with :success
+          should_render_template :new
+        end
+      end
+    end
   end
 end
