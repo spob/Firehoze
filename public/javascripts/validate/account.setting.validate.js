7eb1f2eb2654736f00e2c451b2d7e2a4caece5e4
Rich Sturim <richsturim@rich-sturims-macbookpro.local>
2009-08-11 21:31:39 -0400

    change form submit message, would be better if this could come from i18n files (not sure how to do that though)

--- public/javascripts/validate/account.setting.validate.js
+++ public/javascripts/validate/account.setting.validate.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     $("#avatar_form").validate({
         submitHandler: function(form) {
-            $(form).find(":submit").attr("disabled", true).attr("value", "Submitting...");
+            $(form).find(":submit").attr("disabled", true).attr("value", "Please wait...");
             form.submit();
         }
     })
