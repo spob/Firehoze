642e5ca559cf7b92b0afb7486aa0a43a7e70bc70
Rich Sturim <richsturim@rich-sturims-macbookpro.local>
2009-08-17 07:47:36 -0400

    hide/show loading spinner for ajax'd paging (I know we don't use this -- but, it's good to have an example around)

--- public/javascripts/pagination.js
+++ public/javascripts/pagination.js
@@ -1,7 +1,17 @@
 // public/javascripts/pagination.js
 $(function() {
     $(".pagination a").live("click", function() {
-        $.get(this.href, null, null, "script");
+        var loader = $('#loader')
+        loader.show()   // <-- hidden loader div
+        $.get(
+            this.href,
+            null,
+            function()
+            {
+                loader.hide();
+            },
+            "script"
+            );
         return false;
     })
 });
