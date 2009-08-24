47566b39c603a89eec48c9e1f0e490fd66ae9895
Bob Sturim <bob@sturim.org>
2009-08-23 21:57:04 -0400

    Add unit tests for contacting a user

--- app/controllers/contact_users_controller.rb
+++ app/controllers/contact_users_controller.rb
@@ -5,22 +5,21 @@ class ContactUsersController < ApplicationController
 
   def new
     @to_user = User.find(params[:id])
-    unless @to_user.can_contact?(current_user)
-      flash[:error] = t 'contact_user.cannot_contact'
-      redirect_to home_path
-    end
+    check_permissions
   end
 
   def create
     @to_user = User.find(params[:id])
     @subject = params[:subject]
     @msg = params[:msg]
-    RunOncePeriodicJob.create!(
-            :name => 'Contact User',
-            :job => "ContactUsersController.contact_user(#{@to_user.id}, #{current_user.id}, '#{@subject.gsub(/'/, "")}', #JOBID#)",
-            :data => @msg)
+    if check_permissions
+      RunOncePeriodicJob.create!(
+              :name => 'Contact User',
+              :job => "ContactUsersController.contact_user(#{@to_user.id}, #{current_user.id}, '#{@subject.gsub(/'/, "")}', #JOBID#)",
+              :data => @msg)
       flash[:notice] = t 'contact_user.msg_sent'
       redirect_to user_path(@to_user)
+    end
   end
 
   def self.contact_user(to_user_id, from_user_id, subject, job_id)
@@ -28,5 +27,17 @@ class ContactUsersController < ApplicationController
     to_user = User.find(to_user_id)
     from_user = User.find(from_user_id)
     Notifier.deliver_contact_user(to_user, from_user, subject, job.data)
+  end
+
+  private
+
+  def check_permissions
+    if @to_user.can_contact?(current_user)
+      return true
+    else
+      flash[:error] = t 'contact_user.cannot_contact'
+      redirect_to user_path(@to_user)
+      return false
+    end
   end
 end
--- test/functional/contact_users_controller_test.rb
+++ test/functional/contact_users_controller_test.rb
@@ -1,8 +1,55 @@
-require 'test_helper'
+require File.dirname(__FILE__) + '/../test_helper'
 
 class ContactUsersControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  context "when logged on" do
+    setup do
+      activate_authlogic
+      @user = Factory(:user)
+      UserSession.create(@user)
+    end
+
+    context "sending to a user who disallows contact" do
+      setup { @to_user = Factory.create(:user) }
+
+      context "on GET to :new" do
+        setup { get :new, :id => @to_user }
+
+        should_assign_to :to_user
+        should_respond_with :redirect
+        should_set_the_flash_to /This user's privacy settings prevent you from contacting/
+        should_redirect_to("user show page") { user_path(@to_user) }
+      end
+
+      context "on POST to :create" do
+        setup { post :create, :id => @to_user, :subject => "Blah", :msg => "this is the msg" }
+
+        should_assign_to :to_user
+        should_respond_with :redirect
+        should_set_the_flash_to /This user's privacy settings prevent you from contacting/
+        should_redirect_to("user show page") { user_path(@to_user) }
+      end
+    end
+
+    context "sending to a user who allows contact" do
+      setup { @to_user = Factory.create(:user, :allow_contact => USER_ALLOW_CONTACT_ANYONE) }
+
+      context "on GET to :new" do
+        setup { get :new, :id => @to_user }
+
+        should_assign_to :to_user
+        should_respond_with :success
+        should_not_set_the_flash
+        should_render_template 'new'
+      end
+
+      context "on POST to :create" do
+        setup { post :create, :id => @to_user, :subject => "Blah", :msg => "this is the msg" }
+
+        should_assign_to :to_user
+        should_respond_with :redirect
+        should_set_the_flash_to /message.*sent/
+        should_redirect_to("user show page") { user_path(@to_user) }
+      end
+    end
   end
 end
