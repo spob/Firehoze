5fece817c39f39914706ab3160139fb33843a735
Bob Sturim <bob@sturim.org>
2009-08-25 22:56:53 -0400

    Added show all reviews logic

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,2 +1,11 @@
 module ReviewsHelper
+  def show_all_reviews_link(lesson, user)
+    review_list_count = Review.list_count(lesson, user)
+    if review_list_count > REVIEWS_ON_LESSON_SHOW_PER_PAGE
+      link_to "See all #{pluralize(review_list_count, 'Customer Review')}",
+              lesson_reviews_path(lesson, :per_page => ROWS_PER_PAGE)
+    else
+      ""
+    end
+  end
 end
