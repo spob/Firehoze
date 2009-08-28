c57592b5363729e63eecd17bfb9023aee68363cd
Rich Sturim <richsturim@rich-sturims-macbookpro.local>
2009-08-28 01:50:08 -0400

    messing with animation ... this may be a little too much

--- public/javascripts/jquerytools/scrollable.js
+++ public/javascripts/jquerytools/scrollable.js
@@ -1,9 +1,28 @@
-// execute your scripts when DOM is ready. this is a good habit 
-$(function() {         
-    // initialize scrollable  
-    $("div.scrollable").scrollable({ 
-        size: 4, 
-        items: '#thumbs',   
-        hoverClass: 'hover' 
-    });     
-}); 
+// execute your scripts when DOM is ready. this is a good habit
+$(function() {
+    // initialize scrollable
+    $("div.scrollable").scrollable({
+        size: 4,
+        items: '#thumbs',
+        hoverClass: 'hover',
+
+        // items are auto-scrolled in 2 secnod interval
+        interval: 6000,
+
+        // when last item is encountered go back to first item
+        loop: true,
+
+        // make animation a little slower than the default
+        speed: 900,
+
+        // when seek starts make items little transparent
+        onBeforeSeek: function() {
+            this.getItems().fadeTo(500, 0.9);
+        },
+
+        // when seek ends resume items to full transparency
+        onSeek: function() {
+            this.getItems().fadeTo(400, 1);
+        }
+    });
+});
