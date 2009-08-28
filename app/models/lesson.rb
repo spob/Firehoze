789eb6a3b7450dde7ee9e6b9a143b6019eb9e37a
Bob Sturim <bob@sturim.org>
2009-08-27 22:31:59 -0400

    Remove debug messages left in by mistake Avoid processed videos call because of goofy behavior with polymorphism

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -215,25 +215,19 @@ END
   end
 
   def update_status(unreject=false)
-    puts "=============>#{id}"
     if self.status == LESSON_STATUS_REJECTED and !unreject
-      puts "=============>1"
       # do nothing...moderator put it in this status for a reason
     elsif any_video_match_by_status(VIDEO_STATUS_FAILED)
-      puts "=============>2"
       update_status_attribute(LESSON_STATUS_FAILED)
-    elsif processed_videos.empty? or all_videos_match_by_status(VIDEO_STATUS_PENDING)
-      puts "=============>3 #{processed_videos.empty?} #{all_videos_match_by_status(VIDEO_STATUS_PENDING)}"
+    elsif videos.find_all{|video| video.class == FullProcessedVideo or video.class == PreviewProcessedVideo }.empty? or
+            all_videos_match_by_status(VIDEO_STATUS_PENDING)
       update_status_attribute(LESSON_STATUS_PENDING)
     elsif any_video_match_by_status(LESSON_STATUS_CONVERTING)
-      puts "=============>4"
       update_status_attribute(LESSON_STATUS_CONVERTING)
     elsif all_videos_match_by_status(LESSON_STATUS_READY)
-      puts "=============>5"
       update_status_attribute(LESSON_STATUS_READY)
       Notifier.deliver_lesson_ready(self)
     else
-      puts "=============>6"
       update_status_attribute("Unknown status")
     end
   end
@@ -256,7 +250,7 @@ END
 
   def any_video_match_by_status(status)
     videos.find_all{|video| video.class == FullProcessedVideo or video.class == PreviewProcessedVideo }.each do |video|
-      return true if video.status == status 
+      return true if video.status == status
     end
     return false
   end
