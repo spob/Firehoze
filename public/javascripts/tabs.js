355e53631c8479f3630621c11dcc5a763631c0cf
Rich Sturim <richsturim@rich-sturims-macbookpro.local>
2009-09-01 00:49:07 -0400

    fixes 'Change passwords directs to wrong page'

--- public/javascripts/tabs.js
+++ public/javascripts/tabs.js
@@ -1,14 +1,48 @@
 $(function() {
-    $('#tabs').tabs(
+    $('#lessons_tabs').tabs(
     {
         cache: true,
         cookie: { 
-            expires: 30
+            expires: 30,
+            name: 'lessons_tabs'
         },
         fx: { 
             opacity: 'toggle',
             duration: 300
         }
     });
-
+    $('#lesson_show_tabs').tabs(
+    {
+        cache: true,
+        cookie: { 
+            expires: 30,
+            name: 'lesson_show_tabs'
+        },
+        fx: { 
+            opacity: 'toggle',
+            duration: 300
+        }
+    });
+    $('#account_settings_tabs').tabs(
+    {
+        cookie: { 
+            expires: 30,
+            name: 'account_settings_tabs'
+        },
+        fx: { 
+            opacity: 'toggle',
+            duration: 300
+        },
+    });
+    $('#admin_user_tabs').tabs(
+    {
+        cookie: { 
+            expires: 30,
+            name: 'admin_user_tabs'
+        },
+        fx: { 
+            opacity: 'toggle',
+            duration: 300
+        },
+    });
 });
