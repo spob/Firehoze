56781f39f8a3d6a58813066c0a0ff4169186f3c5
Rich Sturim <richsturim@rich-sturims-macbookpro.local>
2009-09-08 00:57:49 -0400

    don't ask for the jquery-ui javascript file twice (this comes over with jrails

--- vendor/plugins/jrails/init.rb
+++ vendor/plugins/jrails/init.rb
@@ -2,5 +2,6 @@
 #ActionView::Helpers::PrototypeHelper::JQUERY_VAR = 'jQuery'
 
 ActionView::Helpers::AssetTagHelper::JAVASCRIPT_DEFAULT_SOURCES = ['jquery','jquery-ui','jrails']
+# ActionView::Helpers::AssetTagHelper::JAVASCRIPT_DEFAULT_SOURCES = ['jquery','jrails']
 ActionView::Helpers::AssetTagHelper::reset_javascript_include_default
 require 'jrails'
