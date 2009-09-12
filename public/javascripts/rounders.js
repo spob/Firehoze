13e65a47286de7d640e2653d5886896772afdd8f
Rich Sturim <richsturim@rich-sturims-macbookpro.local>
2009-09-12 11:42:06 -0400

    getting the 'Students who bought this video also bought ...' roughed in place

--- public/javascripts/rounders.js
+++ public/javascripts/rounders.js
@@ -3,4 +3,10 @@ $(function() {
         radius: 5,
         background: "white"
     });
+
+    $('#bought_this_video_also_bought').cornerz({
+        radius: 5,
+        background: "white"
+    });
+
 });
