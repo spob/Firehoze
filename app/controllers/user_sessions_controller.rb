fe05fc8b5fe4ffabef1d90d0500253d54b5d7459
Bob Sturim <bob@sturim.org>
2009-09-18 17:55:53 -0400

    Don't show warning message when they log out (if running in locked down mode)

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,18 +1,18 @@
 # This class interactions with the authlogic gem to acutally control logging in and logging out
 class UserSessionsController < ApplicationController
   include SslRequirement
-  
+
   before_filter :require_no_user, :only => [:new, :create]
   before_filter :require_user, :only => :destroy
   ssl_required :create if Rails.env.production?
 
   verify :method => :post, :only => [:create ], :redirect_to => :home_path
   verify :method => :delete, :only => [ :destroy ], :redirect_to => :home_path
-  
+
   def new
     @user_session = UserSession.new
   end
-  
+
   def create
     @user_session = UserSession.new(params[:user_session])
     if @user_session.save
@@ -22,10 +22,15 @@ class UserSessionsController < ApplicationController
       render :action => :new
     end
   end
-  
+
   def destroy
     current_user_session.destroy
     flash[:notice] = t 'login.logout_success'
-    redirect_to root_path
+    flash[:notice] = t 'registration.check_email_for_registration'
+    if APP_CONFIG[CONFIG_ALLOW_UNRECOGNIZED_ACCESS]
+      redirect_to root_path
+    else
+      redirect_to login_path
+    end
   end
 end
