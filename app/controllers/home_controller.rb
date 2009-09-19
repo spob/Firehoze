d537ce6f9ff7f669b8822c66be889a8715f3d971
Bob Sturim <bob@sturim.org>
2009-09-18 22:17:28 -0400

    More code coverage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -11,23 +11,6 @@ class HomeController < ApplicationController
     @user = @current_user
   end
 
-  def update
-    # avoid mass assignment here
-    @user = @current_user
-    @user.email = params[:user][:email].try(:strip)
-    @user.first_name = params[:user][:first_name].try(:strip)
-    @user.last_name = params[:user][:last_name].try(:strip)
-    @user.bio = params[:user][:bio]
-    @user.time_zone = params[:user][:time_zone]
-    @user.language = params[:user][:language]
-    if @user.save
-      flash[:notice] = t 'account_settings.update_success'
-      redirect_to account_url(@user)
-    else
-      render :action => :edit
-    end
-  end
-
   private 
 
   def set_per_page
--- test/functional/home_controller_test.rb
+++ test/functional/home_controller_test.rb
@@ -1,8 +1,21 @@
-require 'test_helper'
+require File.dirname(__FILE__) + '/../test_helper'
 
 class HomeControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+
+  context "when logged on" do
+    setup do
+      activate_authlogic
+      @user = Factory(:user)
+      UserSession.create @user
+    end
+
+    context "on GET to :show" do
+      setup { get :show, :id => @user }
+
+      should_assign_to :user
+      should_respond_with :success
+      should_not_set_the_flash
+      should_render_template "show"
+    end
   end
 end
