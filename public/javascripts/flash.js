8efbf6340603dc654a82daf8d316ad745fb56ac8
Rich Sturim <richsturim@rich-sturims-macbookpro.local>
2009-09-30 00:00:28 -0400

    fix style FLASH message, make it stick around -- and make it look nice

--- public/javascripts/flash.js
+++ public/javascripts/flash.js
@@ -2,9 +2,4 @@ $(function(){
     if ($(".flash").is(":hidden")) {
         $(".flash").slideDown(500);
     }
-    setTimeout(function(){
-        $(".flash").slideUp(750, function () {
-            $(".flash").remove();
-        });
-    }, 3000);
 });
