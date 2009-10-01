1ef9078063cba2766a7d9560122d5ef668049770
Bob Sturim <bob@sturim.org>
2009-09-30 23:12:09 -0400

    Show category in admin lesson list

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -65,7 +65,7 @@ class LessonsController < ApplicationController
 
     def list_admin
       @search = Lesson.search(params[:search])
-      @lessons = @search.paginate :include => [:instructor], :page => params[:page], :per_page => session[:per_page] || ROWS_PER_PAGE
+      @lessons = @search.paginate :include => [:instructor, :category], :page => params[:page], :per_page => session[:per_page] || ROWS_PER_PAGE
     end
 
     def new
