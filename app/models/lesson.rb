cca238edc36c8a9678ed6286fe255168620f71f4
Bob Sturim <bob@sturim.org>
2009-10-03 23:00:52 -0400

    Added periodic job to refresh indexes once a day

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -89,6 +89,7 @@ class Lesson < ActiveRecord::Base
     indexes notes
     indexes instructor.login, :as => :instructor
     indexes category.name, :as => :category
+    has created_at
     set_property :delta => true
     where "status = 'Ready'"
     group_by "instructor_id"
