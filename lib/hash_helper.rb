fd778c207052f4ecf47cc2146606bab0b14002df
Bob Sturim <bob@sturim.org>
2009-10-09 20:26:12 -0400

    Add a strip to registration code

--- lib/hash_helper.rb
+++ lib/hash_helper.rb
@@ -2,7 +2,7 @@ require 'digest'
 
 module HashHelper
   def formatted_hash(plain_text, prefix="", suffix="")
-    hash = gen_hash(plain_text.downcase, prefix, suffix)
+    hash = gen_hash(plain_text.downcase.strip, prefix, suffix)
     hash[0, 4] + "-" + hash[4, 4] + "-" + hash[8, 4] + "-" + hash[12, 4] + "-" + 
             hash[16, 4] + "-" + hash[20, 4] + "-" + hash[24, 4] + "-" + hash[28, 4]
   end
