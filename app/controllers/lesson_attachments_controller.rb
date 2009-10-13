03c5817401f97cebf7ca441e0085463e7fa01b4b
Bob Sturim <bob@sturim.org>
2009-10-12 22:05:11 -0400

    Don't throw exception if validation fails

--- app/controllers/lesson_attachments_controller.rb
+++ app/controllers/lesson_attachments_controller.rb
@@ -14,7 +14,7 @@ class LessonAttachmentsController < ApplicationController
     if has_access? @lesson
       @attachment = LessonAttachment.new( params[:attachment] )
       @attachment.lesson = @lesson
-      if @attachment.save!
+      if @attachment.save
         flash[:notice] = t 'attachment.create_success'
         redirect_to lesson_path(@lesson)
       else
