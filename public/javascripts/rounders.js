f01f3f4ed05835f36a8bbaee41089d46ef535401
Rich Sturim <richsturim@rich-sturims-macbookpro.local>
2009-10-31 04:37:32 -0400

    vimeo nav, not rendering in all browsers

--- public/javascripts/rounders.js
+++ public/javascripts/rounders.js
@@ -5,10 +5,29 @@ $(function() {
     });
 
     $('#tag_cloud, #bought_this_video_also_bought, #what_do_students_ultimately').cornerz({
-        radius: 5,
+        radius: 5
     });
 
     $('#side-box').cornerz({
-        radius: 3,
+        radius: 3
     });
+
+    $('#side-box').cornerz({
+        radius: 3
+    });
+
+
+//     $('ul.topnav').cornerz({
+//         radius: 12, 
+// corners: "bl br",
+// background: "#2D2B2C"
+// 
+//     });
+
+
+
+// $('ul.subnav').cornerz({
+// 		//     radius: 3,
+// 		// corners: "bl br"
+// });
 });
