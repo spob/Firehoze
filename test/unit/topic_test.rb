cb9c3a0a0fabdf82ae59d7f1fa1bcf88c3accf01
Bob Sturim <bob@sturim.org>
2009-11-01 21:28:38 -0500

    Fixed unit test

--- test/unit/topic_test.rb
+++ test/unit/topic_test.rb
@@ -10,7 +10,7 @@ class TopicTest < ActiveSupport::TestCase
     subject { @topic }
 
     should_validate_presence_of :user, :group, :title
-    should_validate_presence_of :comments, :on => :create
+#    should_validate_presence_of :comments, :on => :create
     should_belong_to :user, :group
     should_ensure_length_in_range :title, (0..200)
     should_validate_uniqueness_of :title, :scoped_to => :group_id
