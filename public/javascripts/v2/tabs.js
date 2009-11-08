6450a52ff0fdb029209c1ca52a3dd59e102a7e7b
Rich Sturim <rich@sturim.org>
2009-11-07 22:09:18 -0500

    roughing in nested tabs

--- public/javascripts/v2/tabs.js
+++ public/javascripts/v2/tabs.js
@@ -1,4 +1,10 @@
 // setup ul.tabs to work as tabs for each div directly under div.panes 
 $(function() {
-    $(".sub-navigation ul.buttons").tabs("div.panes > div");
+    $(".sub-navigation ul.buttons").tabs("div.sub-navigation-panes > div", {
+        effect: 'fade'
+    });
+
+    $(".sub-sub-navigation ul.buttons").tabs("div.sub-sub-navigation-panes > div", {
+        effect: 'fade'
+    });
 });
