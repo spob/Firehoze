f3cc928fc82732e6e21157c5bdd581b29cb13c80
Rich Sturim <rich@sturim.org>
2009-11-08 08:30:30 -0500

    more tabs styling on 'my_firehoze' page

--- public/javascripts/v2/tabs.js
+++ public/javascripts/v2/tabs.js
@@ -1,10 +1,15 @@
-// setup ul.tabs to work as tabs for each div directly under div.panes 
 $(function() {
-    $(".sub-navigation ul.buttons").tabs("div.sub-navigation-panes > div", {
+
+    $("#my_stuff div.sub-navigation > ul.buttons").tabs("#my_stuff > div.sub-navigation-panes > div", {
         effect: 'fade'
     });
 
-    $(".sub-sub-navigation ul.links").tabs("div.sub-sub-navigation-panes > div", {
+    $("#my_stuff div.sub-sub-navigation > ul.links").tabs("#my_stuff div.sub-sub-navigation-panes > div", {
         effect: 'fade'
     });
+
+    $("#account_history div.sub-navigation > ul.buttons").tabs("#account_history > div.sub-navigation-panes > div", {
+        effect: 'fade'
+    });
+
 });
