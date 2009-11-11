1a9441ccd72926f0a16b9ba872b44ec222e7062e
Bob Sturim <bob@sturim.org>
2009-11-10 21:04:52 -0500

    Added unit tests

--- test/functional/topic_comments_controller_test.rb
+++ test/functional/topic_comments_controller_test.rb
@@ -25,13 +25,70 @@ class TopicCommentsControllerTest < ActionController::TestCase
       end
 
       context "on GET to :new" do
-      setup { get :new, :topic_id => @topic.id }
+        setup { get :new, :topic_id => @topic.id }
 
         should_assign_to :topic
         should_assign_to :topic_comment
         should_respond_with :success
         should_not_set_the_flash
         should_render_template "new"
+      end
+    end
+
+    fast_context "with a topic comment defined" do
+      setup { @topic_comment = Factory.create(:topic_comment) }
+
+      fast_context "with moderator access" do
+        setup do
+          @user.has_role 'moderator'
+          assert @user.is_moderator?
+        end
+
+        fast_context "on GET to :edit" do
+          setup { get :edit, :id => @topic_comment }
+
+          should_assign_to :topic_comment
+          should_respond_with :success
+          should_not_set_the_flash
+          should_render_template "edit"
+        end
+
+        fast_context "on PUT to :update" do
+          setup { put :update, :id => @topic_comment, :topic => @topic_comment.topic,
+                      :topic_comment => { :public => true } }
+
+          should_set_the_flash_to :topic_comment_update_success
+          should_assign_to :topic_comment
+          should_respond_with :redirect
+          should_redirect_to("Topic Show") { topic_path(@topic_comment.topic) }
+        end
+      end
+
+      fast_context "without moderator access" do
+        setup { @user.has_no_role 'moderator' }
+
+        fast_context "on GET to :edit" do
+          setup do
+            assert !@user.is_moderator?
+            assert !@topic_comment.can_edit?(@user)
+            get :edit, :id => @topic_comment
+          end
+
+          should_respond_with :redirect
+          should_set_the_flash_to /You do not have rights/
+          should_redirect_to("Topic Show") { topic_path(@topic_comment.topic) }
+        end
+
+        fast_context "on PUT to :update" do
+          setup do
+            put :update, :id => @topic_comment, :topic => @topic_comment.topic,
+                :topic_comment => { :public => true }
+          end
+
+          should_set_the_flash_to /You do not have rights/
+          should_respond_with :redirect
+          should_redirect_to("Topic Show") { topic_path(@topic_comment.topic) }
+        end
       end
     end
   end
