703c2bfc5998685cc1656aa5b4c9cc49cd791238
Rich Sturim <rich@sturim.org>
2009-11-16 22:54:49 -0500

    move all js and css into a 'v2' folder

---
+++ public/javascripts/v2/rounders.js
@@ -0,0 +1,23 @@
+$(function() {
+    $('#marketing_fpo').cornerz({
+        radius: 5,
+        background: "white"
+    });
+
+    $('#tag_cloud, #bought_this_video_also_bought, #what_do_students_ultimately').cornerz({
+        radius: 5
+    });
+
+    $('#side-box').cornerz({
+        radius: 6
+    });
+
+    $('.box').cornerz({
+        radius: 5
+    });
+
+    $('.inner_box').cornerz({
+        radius: 5,
+				background : "#EDEDED"
+    });
+});
