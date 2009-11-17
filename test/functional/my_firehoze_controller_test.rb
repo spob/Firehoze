2f99fa8aad8e9db444ef2b4e3d36187c63a3cc1d
Bob Sturim <bob@sturim.org>
2009-11-16 22:47:30 -0500

    Added unit test for my_firehoze

--- test/functional/my_firehoze_controller_test.rb
+++ test/functional/my_firehoze_controller_test.rb
@@ -3,14 +3,14 @@ require 'fast_context'
 
 class MyFirehozeControllerTest < ActionController::TestCase
 
-  context "when logged on" do
+  fast_context "when logged on" do
     setup do
       activate_authlogic
       @active_user = Factory(:user)
       UserSession.create @active_user
     end
 
-    context "on GET to :index" do
+    fast_context "on GET to :index" do
       setup { get :index }
 
       should_assign_to :user
@@ -21,15 +21,40 @@ class MyFirehozeControllerTest < ActionController::TestCase
       should_render_template "index"
     end
 
-    context "on GET to :my_stuff" do
+    fast_context "on GET to :my_stuff" do
       setup { get :my_stuff }
 
       should_assign_to :user
-#      should_assign_to :activities
-#      should_assign_to :tweets
+      should_assign_to :latest_browsed
+      should_assign_to :owned
+      should_assign_to :wishlist
+      should_assign_to :reviews
+      should_assign_to :groups 
+      should_assign_to :followed_instructors     
       should_respond_with :success
       should_not_set_the_flash
-      should_render_template "index"
+      should_render_template "my_stuff"
+    end
+
+    fast_context "on GET to :instructor" do
+      setup { get :instructor }
+
+      should_assign_to :instructed_lessons
+      should_assign_to :students
+      should_assign_to :payments     
+      should_respond_with :success
+      should_not_set_the_flash
+      should_render_template "instructor"
+    end
+
+    fast_context "on GET to :account_history" do
+      setup { get :account_history }
+
+      should_assign_to :orders
+      should_assign_to :credits
+      should_respond_with :success
+      should_not_set_the_flash
+      should_render_template "account_history"
     end
   end
 end
