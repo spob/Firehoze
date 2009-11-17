c14cd2342165f0979d9b668559fd05b66aa3dd65
Rich Sturim <rich@sturim.org>
2009-11-16 22:24:25 -0500

    fix spinner

--- public/javascripts/pagination.js
+++ public/javascripts/pagination.js
@@ -1,6 +1,6 @@
 $(function() {
     $(".pagination a, .filter_activity_by a").live("click", function() {
-        var loader = $('#loader')
+        var loader = $('.loader')
         loader.show()   // <-- hidden loader div
         $.get(this.href, null,
             function()
