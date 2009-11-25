9def8b64d5b3e929ee1bf5c1b505a7e4916e959b
Rich Sturim <rich@sturim.org>
2009-11-25 12:37:29 -0500

    another fix to an extra pipe in nav list (ie only)

--- public/javascripts/v2/pagination.js
+++ public/javascripts/v2/pagination.js
@@ -1,5 +1,5 @@
 $(function() {
-    $(".pagination a, .filter_activity_by a").live("click", function() {
+    $(".pagination a, .filter-activity-by a").live("click", function() {
         var loader = $('.loader')
         loader.show()   // <-- hidden loader div
         $.getScript(this.href, function() { loader.hide(); });
