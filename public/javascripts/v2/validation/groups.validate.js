115ccafd086b7244d977523d873b5044d124fafe
Rich Sturim <rich@sturim.org>
2009-12-06 13:55:30 -0500

    get char counting introduced into groups

--- public/javascripts/v2/validation/groups.validate.js
+++ public/javascripts/v2/validation/groups.validate.js
@@ -8,17 +8,12 @@ $(document).ready(function() {
         }
     })
 
-// $('#lesson_title').simplyCountable({
-//     counter: '#lesson_title_counter',
-//     countType: 'characters',
-//     maxCount: 50,
-//     countDirection: 'down'
-// });
-//
-// $('#lesson_synopsis').simplyCountable({
-//     counter: '#lesson_synopsis_counter',
-//     countType: 'characters',
-//     maxCount: 500,
-//     countDirection: 'down'
-// });
+    $('#group_name').simplyCountable({
+        counter: '#group_name_counter',
+        countType: 'characters',
+        maxCount: 50,
+        countDirection: 'down',
+        safeClass: 'safe',
+        overClass: 'over'
+    });
 });
