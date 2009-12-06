54221aaa3360b9257cf83a80575b1bb62315ebc6
Bob Sturim <bob@sturim.org>
2009-12-06 17:55:51 -0500

    Added client-side validation for gift certificates

---
+++ public/javascripts/v2/validation/gift_certificates.validate.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $("#gift_certificate").validate({
+        errorElement: "span",
+        success: "valid",
+        submitHandler: function(form) {
+            $(form).find(":submit").attr("disabled", true).attr("value", "Please wait ...");
+            form.submit();
+        }
+    })
+});
