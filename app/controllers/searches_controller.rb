478f65f4867b3fbcb7d43324b6ee603de7a6f0ae
Rich Sturim <rich@sturim.org>
2009-12-08 22:42:29 -0500

    initial commit on restyling the basic search results

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,27 +1,30 @@
 class SearchesController < ApplicationController
+
+  layout 'application_v2'
+
   def index
     @search_criteria = params[:search_criteria]
-    @lesson_format = 'narrow'
+    @lesson_format = 'brief'
     @collection = 'search'
     # using paginate to set a maximum # of rows returned
     @lessons = Lesson.search @search_criteria,
-                             :conditions => { :status => 'Ready'},
-                             :include => :instructor,
-                             :page => 1,
-                             :per_page => 25,
-                             :retry_stale => true
+      :conditions => { :status => 'Ready'},
+      :include => :instructor,
+      :page => 1,
+      :per_page => 25,
+      :retry_stale => true
 
-#    member_groups = [-1] + (current_user ? current_user.member_groups : [])
+    #    member_groups = [-1] + (current_user ? current_user.member_groups : [])
     @groups = Group.search @search_criteria,
-                           :conditions => { :private => 0},
-                           :include => [:owner ],
-                           :page => 1,
-                           :per_page => 25,
-                           :retry_stale => true
+      :conditions => { :private => 0},
+      :include => [:owner ],
+      :page => 1,
+      :per_page => 25,
+      :retry_stale => true
 
     @users = User.search @search_criteria,
-                         :page => 1,
-                         :per_page => 25,
-                         :retry_stale => true
+      :page => 1,
+      :per_page => 25,
+      :retry_stale => true
   end
 end
