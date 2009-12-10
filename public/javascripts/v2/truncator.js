bb631f4b95ce979ff476ac9b5e3f846becd1d07f
Rich Sturim <rich@sturim.org>
2009-12-09 23:54:07 -0500

    getting user and groups better formatted for search results

--- public/javascripts/v2/truncator.js
+++ public/javascripts/v2/truncator.js
@@ -6,6 +6,13 @@ $(function() {
         fade : 500
     });
 
+    $('.group-description').truncate({
+        max_length: 360,
+        more : "more",
+        less : "less",
+        fade : 500
+    });
+
     $('.review-body').truncate({
         max_length: 300,
         more : "more",
