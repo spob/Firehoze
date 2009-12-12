e6adb089f3a6366fb37adb3a4f29fb72bb8c2464
Bob Sturim <bob@sturim.org>
2009-12-11 22:03:37 -0500

    Added pagination to search pages

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -9,21 +9,21 @@ class SearchesController < ApplicationController
     @lessons = Lesson.search @search_criteria,
       :conditions => { :status => 'Ready'},
       :include => :instructor,
-      :page => 1,
-      :per_page => 25,
+      :page => params[:lesson_page],
+      :per_page => 2,
       :retry_stale => true
 
     # member_groups = [-1] + (current_user ? current_user.member_groups : [])
     @groups = Group.search @search_criteria,
       :conditions => { :private => 0},
       :include => [:owner ],
-      :page => 1,
-      :per_page => 25,
+      :page => params[:group_page],
+      :per_page => 2,
       :retry_stale => true
 
     @users = User.search @search_criteria,
-      :page => 1,
-      :per_page => 25,
+      :page => params[:user_page],
+      :per_page => 2,
       :retry_stale => true
   end
 end
