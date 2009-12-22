9013c5eb809eefe02cc36f170e0312ce99aefc3f
Bob Sturim <bob@sturim.org>
2009-12-21 22:47:56 -0500

    Added webrat integration test for search

---
+++ test/integration/search_test_test.rb
@@ -0,0 +1,34 @@
+require File.dirname(__FILE__) + '/../test_helper'
+
+class SearchTestTest < ActionController::IntegrationTest
+  fixtures :all
+  context "given a user" do
+    setup do
+      @user = Factory.create(:user)
+    end
+
+    context "Given some lessons" do
+      setup do
+        @lesson1 = Factory.create(:lesson, :title => "test1")
+        @lesson2 = Factory.create(:lesson, :title => "test2")
+        @lesson3 = Factory.create(:lesson, :title => "blah")
+        @lesson1.update_attribute(:status, LESSON_STATUS_READY)
+        @lesson2.update_attribute(:status, LESSON_STATUS_READY)
+        @lesson3.update_attribute(:status, LESSON_STATUS_READY)
+        assert @lesson1.ready?
+      end
+
+      should "find lessons" do
+        visit login_url
+        fill_in "user_session[login]", :with => @user.login
+        fill_in "user_session[password]", :with => "xxxxx"
+        click_button "Login"
+        assert_contain "my firehoze"
+        fill_in "search_criteria", :with => "test"
+        click_button 'searchbox_submit'
+        assert_contain "Search Results"
+        assert_contain "no results"
+      end
+    end
+  end
+end
