7ea2813c6adc508defb884738448a1a4fb005b2e
Bob Sturim <bob@sturim.org>
2009-12-26 21:26:19 -0500

    Format categories

---
+++ public/javascripts/v2/validation/categories.validate.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    $("#edit_category").validate({
+        errorElement: "span",
+        success: "valid",
+        submitHandler: function(form) {
+            $(form).find(":submit").attr("disabled", true).attr("value", "Please wait ...");
+            form.submit();
+        }
+    })
+
+    $('#group_name').simplyCountable({
+        counter: '#group_name_counter',
+        countType: 'characters',
+        maxCount: 50,
+        countDirection: 'down',
+        safeClass: 'safe',
+        overClass: 'over'
+    });
+});
