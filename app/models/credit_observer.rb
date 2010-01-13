28d3f682ef1c986e8b024672bbc2ac63ea04bab8
Bob Sturim <bob@sturim.org>
2010-01-13 07:31:31 -0500

    Fixed activity for lesson purchases...Rich, try it again

--- app/models/credit_observer.rb
+++ app/models/credit_observer.rb
@@ -1,6 +1,6 @@
 class CreditObserver < ActiveRecord::Observer
   def after_save(credit)
-    if credit.activity_compiled_at.nil? and credit.redeemed_at
+    if credit.activity_compiled_at.nil? and credit.lesson
       Credit.transaction do
         credit.compile_activity
       end
