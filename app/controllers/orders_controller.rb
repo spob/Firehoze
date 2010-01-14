05df16b766a0a67d6e486998a7628d3b86ca4dcf
Rich Sturim <rich@sturim.org>
2010-01-14 05:25:48 -0500

    user the correct layout for a regular user, when viewing the ordershow'

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -101,7 +101,7 @@ class OrdersController < ApplicationController
   def layout_for_action
     if %w(index).include?(params[:action]) || (params[:action] == 'show' and current_user.is_admin?)
       'admin_v2'
-    elsif %w(new create update).include?(params[:action])
+    elsif %w(create new show update).include?(params[:action])
       'application_v2'
     else
       'application'
