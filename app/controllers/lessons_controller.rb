3b64a74a65b83c87c7baa812fa5f713f63bd9387
Rich Sturim <rich@sturim.org>
2010-01-17 08:47:35 -0500

    make unrecognized user page point to new layout (application_v2)

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -25,6 +25,7 @@ class LessonsController < ApplicationController
   @@free_download_counts = [ 0, 5, 10, 25 ]
 
 
+  # Unrecognized user page
   def index
     if params[:reset] == "y"
       # clear category browsing
@@ -339,7 +340,7 @@ class LessonsController < ApplicationController
       'content_in_tab'
     elsif %w(list_admin).include?(params[:action])
       'admin_v2'
-    elsif %w(advanced_search create edit lesson_notes list new perform_advanced_search recommend show show_groups stats tagged_with update).include?(params[:action])
+    elsif %w(advanced_search create edit index lesson_notes list new perform_advanced_search recommend show show_groups stats tagged_with update).include?(params[:action])
       'application_v2'
     else
       'application'
