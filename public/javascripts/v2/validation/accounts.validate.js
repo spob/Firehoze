e9d577918205ae85eb98cb15344354adbd101da0
Bob Sturim <bob@sturim.org>
2010-01-23 00:53:33 -0500

    Backed out my work on the instructor wizards

--- public/javascripts/v2/validation/accounts.validate.js
+++ public/javascripts/v2/validation/accounts.validate.js
@@ -7,7 +7,8 @@ $(document).ready(function() {
             form.submit();
         }
     });
-    
+
+ $(document).ready(function() {
     $("#password_form").validate({
         errorElement: "span",
         success: "valid",
@@ -17,7 +18,7 @@ $(document).ready(function() {
         }
     });
 
-    $("#author, #instructor_address").validate({
+    $("#author").validate({
         errorElement: "span",
         success: "valid",
         submitHandler: function(form) {
@@ -85,60 +86,6 @@ $(document).ready(function() {
         counter: '#last_name_counter',
         countType: 'characters',
         maxCount: 40,
-        countDirection: 'down',
-        safeClass: 'safe',
-        overClass: 'over'
-    });
-
-    $('#user_billing_name').simplyCountable({
-        counter: '#user_billing_name_counter',
-        countType: 'characters',
-        maxCount: 100,
-        countDirection: 'down',
-        safeClass: 'safe',
-        overClass: 'over'
-    });
-
-    $('#user_address1').simplyCountable({
-        counter: '#user_address1_counter',
-        countType: 'characters',
-        maxCount: 150,
-        countDirection: 'down',
-        safeClass: 'safe',
-        overClass: 'over'
-    });
-
-    $('#user_address2').simplyCountable({
-        counter: '#user_address2_counter',
-        countType: 'characters',
-        maxCount: 150,
-        countDirection: 'down',
-        safeClass: 'safe',
-        overClass: 'over'
-    });
-
-    $('#user_city').simplyCountable({
-        counter: '#user_city_counter',
-        countType: 'characters',
-        maxCount: 50,
-        countDirection: 'down',
-        safeClass: 'safe',
-        overClass: 'over'
-    });
-
-    $('#user_state').simplyCountable({
-        counter: '#user_state_counter',
-        countType: 'characters',
-        maxCount: 50,
-        countDirection: 'down',
-        safeClass: 'safe',
-        overClass: 'over'
-    });
-
-    $('#user_zip').simplyCountable({
-        counter: '#user_zip_counter',
-        countType: 'characters',
-        maxCount: 25,
         countDirection: 'down',
         safeClass: 'safe',
         overClass: 'over'
