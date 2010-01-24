944ebf37664c27bfc768bb4e96ae4bb87162be78
Bob Sturim <bob@sturim.org>
2010-01-23 20:00:02 -0500

    Fixed unit tests

--- test/unit/helpers/lesson_comments_helper_test.rb
+++ test/unit/helpers/lesson_comments_helper_test.rb
@@ -19,7 +19,7 @@ class LessonCommentsHelperTest < ActionView::TestCase
       end
 
       should "return empty link" do
-        assert_equal "<a href=\"/lessons/#{@lesson.id}-#{@lesson.title.gsub(/ /, "-").downcase}/lesson_comments?per_page=10\">See all #{@lesson.comments.size} Comments</a>", 
+        assert_equal "<a href=\"/lessons/#{@lesson.id}-#{@lesson.title.gsub(/ /, "-").downcase}/lesson_comments\">See all #{@lesson.comments.size} Comments</a>", 
                      show_all_lesson_comments_link(@lesson, nil)
       end
     end
