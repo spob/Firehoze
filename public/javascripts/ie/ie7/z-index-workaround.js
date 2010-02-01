a84ad59fcf9ce4a6c911bb55aaba83ebd5b17144
Rich Sturim <rich@sturim.org>
2010-02-01 09:23:37 -0500

    wrapping this up for now

--- public/javascripts/ie/ie7/z-index-workaround.js
+++ public/javascripts/ie/ie7/z-index-workaround.js
@@ -1,15 +1,15 @@
 $(function() {
     var zIndexNumber = 1000;
-    $('div').each(function() {
+
+    $('div:not("div.lesson-tooltip")').each(function() {
         $(this).css('zIndex', zIndexNumber);
-        zIndexNumber -= 10;
+          zIndexNumber -= 10;
         // console.log("%o",this);
     });
 
     // handle GetSatisfaction and others
     $('div#fdbk_overlay').css('zIndex', 2000);
-    $('div.lesson-tooltip').css('zIndex', 2000);
+    $('div#top-navigation-container').css('zIndex', 1);
+    $('div.lesson-tooltip').css('zIndex', 3000);
     $('div.pagination').css('zIndex', 1);
-    $('div.small-lesson-container').css('zIndex', 1);
-    $('div#top-navigation-container').css('zIndex', 1999);
 });
