71cde5a44095c6e59abf67dd975da56ffaf25c83
Rich Sturim <rich@sturim.org>
2010-02-01 10:51:34 -0500

    move 'ACCOUNT SETTINGS' into the 'My Firehoze' area & fix numerous javascript errors

--- public/javascripts/accounts.js
+++ public/javascripts/accounts.js
@@ -54,3 +54,44 @@ $(function() {
     });
 
 });
+
+
+$(function() {
+
+  $('#user_login').simplyCountable({
+    counter: '#login_counter',
+    countType: 'characters',
+    maxCount: 25,
+    countDirection: 'down',
+    safeClass: 'safe',
+    overClass: 'over'
+  });
+
+  $('#user_email').simplyCountable({
+    counter: '#email_counter',
+    countType: 'characters',
+    maxCount: 100,
+    countDirection: 'down',
+    safeClass: 'safe',
+    overClass: 'over'
+  });
+
+  $('#user_first_name').simplyCountable({
+    counter: '#first_name_counter',
+    countType: 'characters',
+    maxCount: 40,
+    countDirection: 'down',
+    safeClass: 'safe',
+    overClass: 'over'
+  });
+
+  $('#user_last_name').simplyCountable({
+    counter: '#last_name_counter',
+    countType: 'characters',
+    maxCount: 40,
+    countDirection: 'down',
+    safeClass: 'safe',
+    overClass: 'over'
+  });
+
+});
