62c43325d988a03f2386bcad2ab2bd186dbfe035
Rich Sturim <rich@sturim.org>
2010-02-03 10:25:10 -0500

    let's start using the minified validation script everywhere

--- public/javascripts/validate/account.setting.validate.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $("#avatar_form").validate({
-        submitHandler: function(form) {
-            $(form).find(":submit").attr("disabled", true).attr("value", "Please wait...");
-            form.submit();
-        }
-    })
-});
--- public/javascripts/validate/flag.validate.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $("#flag").validate({
-        submitHandler: function(form) {
-            $(form).find(":submit").attr("disabled", true).attr("value", "Please wait...");
-            form.submit();
-        }
-    })
-});
