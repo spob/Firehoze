06ca93beedda2f0dee684370db22ef6603b88b61
Bob Sturim <bob@sturim.org>
2010-02-05 12:59:14 -0500

    Fixed migration

--- db/migrate/20100205165732_populate_instruction_notified_at.rb
+++ db/migrate/20100205165732_populate_instruction_notified_at.rb
@@ -1,13 +1,13 @@
 class PopulateInstructionNotifiedAt < ActiveRecord::Migration
   def self.up
     User.active.instructors.instructor_signup_notified_at_null.each do |u|
-      u.update_attribute(:instructor_signup_notified_at, u.author_agreement_accepted_on)
+      execute("update users set instructor_signup_notified_at = '#{u.author_agreement_accepted_on.to_s(:db)}' where id = #{u.id}")
     end
   end
 
   def self.down
     User.active.instructors.instructor_signup_notified_at_not_null.each do |u|
-      u.update_attribute(:instructor_signup_notified_at, nil)
+      execute("update users set instructor_signup_notified_at = null where id = #{u.id}")
     end
   end
 end
