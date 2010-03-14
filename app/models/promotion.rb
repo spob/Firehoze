660b5dea8751fad6d0baf09662f14c111bee3714
Bob Sturim <bob@sturim.org>
2010-03-14 17:31:03 -0400

    Added unit tests

--- app/models/promotion.rb
+++ app/models/promotion.rb
@@ -3,7 +3,13 @@ class Promotion < ActiveRecord::Base
   validates_uniqueness_of :code, :case_sensitive => false
   validates_length_of :code, :maximum => 15, :allow_nil => true
   validates_length_of :promotion_type, :maximum => 50, :allow_nil => true
-#  validates_length_of :last_name, :maximum => 40, :allow_nil => true
-#  validates_length_of :first_name, :maximum => 40, :allow_nil => true
-#  validates_length_of :login, :maximum => 25, :allow_nil => true
+
+  before_validation :strip_white
+
+
+  private
+
+  def strip_white
+    self.code = code.strip unless code.nil?
+  end
 end
--- test/unit/promotion_test.rb
+++ test/unit/promotion_test.rb
@@ -6,11 +6,24 @@ class PromotionTest < ActiveSupport::TestCase
   fast_context "given a promotion" do
     setup do
       @promotion = Factory.create(:promotion)
+      assert @promotion.expires_at.present?
     end
 
-    should_validate_presence_of :code, :promotion_type, :expires_at
-    should_validate_uniqueness_of :code
-    should_ensure_length_in_range :code, (0..15)
-    should_ensure_length_in_range :promotion_type, (0..50)
+    subject { @promotion }
+
+    context "test attributes" do
+      subject { @promotion }
+      
+      should_validate_presence_of :code, :promotion_type, :expires_at
+      should_validate_uniqueness_of :code
+      should_ensure_length_in_range :code, (0..15)
+      should_ensure_length_in_range :promotion_type, (0..50)
+    end
+
+    should "strip white space from code" do
+      @promotion.code = "  xxxx  "
+      @promotion.save!
+      assert_equal "xxxx", @promotion.code
+    end
   end
 end
