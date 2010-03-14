d053671bc20e053cebd2a146c27c5462ba456b3f
Bob Sturim <bob@sturim.org>
2010-03-14 14:02:19 -0400

    Created new table for promotions

---
+++ db/migrate/20100314175536_create_promotions.rb
@@ -0,0 +1,17 @@
+class CreatePromotions < ActiveRecord::Migration
+  def self.up
+    create_table :promotions do |t|
+      t.string :code, :null => false, :limit => 15
+      t.string :promotion_type, :null => false, :limit => 50
+      t.datetime :expires_at, :null => false
+      t.text :description, :null => true
+
+      t.timestamps
+    end  
+    add_index :promotions, :code, :unique => true
+  end
+
+  def self.down
+    drop_table :promotions
+  end
+end
