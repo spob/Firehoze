51ffd49862d2cd379db5b930fde84c200bf41caf
Bob Sturim <bob@sturim.org>
2010-03-31 06:54:37 -0400

    More fb work

--- config/routes.rb
+++ config/routes.rb
@@ -28,7 +28,7 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :carts, :only => [ :show ]
   map.resources :categories, :collection => { :explode => :post, :list_admin => :get }
   map.resources :contact_users, :only => [ :create, :new ]
-#  map.resources :facebooks, :only => [ :index ]
+  map.resources :facebooks, :only => [ :index ]
 #  map.resources :credits
   map.resources :flags, :only => [ :new, :create, :index, :show, :update, :edit ]
   map.check_gift_code "gift_certificates/check_gift_certificate_code", :controller => "gift_certificates", :action => "check_gift_certificate_code"
