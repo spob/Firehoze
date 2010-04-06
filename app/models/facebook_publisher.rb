6b6f09f2bf145d151691d0be588e5fc775aa237f
Bob Sturim <bob@sturim.org>
2010-04-05 23:07:01 -0400

    Got publishing to wall posting towork

--- app/models/facebook_publisher.rb
+++ app/models/facebook_publisher.rb
@@ -5,12 +5,28 @@ class FacebookPublisher < Facebooker::Rails::Publisher
   end
 
   def group_comment(comment)
-    send_as :user_action
+    send_as :publish_stream
     from comment.user.facebook_session.user
-    story_size SHORT
-    data :images=>[image(comment.topic.group.logo.url(:medium),
-                         group_url(comment.topic.group))],
-                   :actor => comment.user.login, :comment=>comment.body,
-                   :comment_url=>topic_url(comment.topic), :group=>comment.topic.group.name
+    target comment.user.facebook_session.user
+    message "added the comment #{truncate(comment.body, 50)} to the Firehoze group #{comment.topic.group.name}"
+    action_links [ action_link("View On Firehoze", absolute_path(:controller => 'topics', :action => 'show', :id => comment.topic)) ]
+                                           
+#    send_as :user_action
+#    from comment.user.facebook_session.user
+#    story_size SHORT
+#    #:images=>[ image(comment.topic.group.logo.url(:medium), group_url(comment.topic.group))],
+#    data :actor => comment.user.login, :comment=>comment.body,
+#         :comment_url=>topic_url(comment.topic, :canvas => false), :group=>comment.topic.group.name
+  end
+
+  private
+
+  def truncate(txt, len)
+    txt = "#{txt[0, len]}..." if txt.length > len
+    txt
+  end
+
+  def absolute_path(options)
+    "#{APP_CONFIG[CONFIG_PROTOCOL]}://#{APP_CONFIG[CONFIG_HOST]}:#{APP_CONFIG[CONFIG_PORT]}#{url_for(options.merge({:only_path => true, :skip_relative_url_root => true}))}"
   end
 end
