3b8231fbbb60216d87533f1534d2ee74d170e06e
Bob Sturim <bob@sturim.org>
2010-04-06 20:29:34 -0400

    Length change for publisher

--- app/models/facebook_publisher.rb
+++ app/models/facebook_publisher.rb
@@ -4,7 +4,7 @@ class FacebookPublisher < Facebooker::Rails::Publisher
     send_as :publish_stream
     from comment.user.facebook_session.user
     target comment.user.facebook_session.user
-    message "added the comment '#{truncate(comment.body, 50)}' to the topic #{comment.topic.title}"
+    message "added the comment '#{truncate(comment.body, 100)}' to the topic #{comment.topic.title}"
     action_links [ action_link("View On Firehoze", absolute_path(:controller => 'topics', :action => 'show', :id => comment.topic)) ]
     attachment :name => "#{comment.topic.group.name}", :href => absolute_path(:controller => 'groups', :action => 'show', :id => comment.topic.group), :description => "Firehoze Groups", :media => [{:type => 'image', :src => Group.convert_logo_url_to_cdn(comment.topic.group.logo.url(:medium), :cdn), :href => absolute_path(:controller => 'groups', :action => 'show', :id => comment.topic.group)}]  
   end
