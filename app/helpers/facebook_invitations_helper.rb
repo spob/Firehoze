ce8191c2b9325a6b6225728a5eca9be3bf07fbad
Bob Sturim <bob@sturim.org>
2010-04-08 22:24:34 -0400

    Don't show invitations to users who already belong

--- app/helpers/facebook_invitations_helper.rb
+++ app/helpers/facebook_invitations_helper.rb
@@ -1,2 +1,7 @@
 module FacebookInvitationsHelper
+  def exclude_ids(friends)
+    friend_ids = friends.split(/,/)
+    existing_users = User.facebook_id_not_null.collect{|u| u.facebook_id.to_s}
+    friend_ids & existing_users
+  end
 end
