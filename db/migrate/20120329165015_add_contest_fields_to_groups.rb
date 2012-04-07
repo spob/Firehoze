56366ca244e69d9dc6466dc745622ec82714a562
Peter Wiley-Cordone <pcordone@gmail.com>
2012-04-06 20:24:44 -0400

    Added new migrations that RubyMine missed. Added wireframes and PDF of wiki dev setup instructions.

---
+++ db/migrate/20120329165015_add_contest_fields_to_groups.rb
@@ -0,0 +1,13 @@
+class AddContestFieldsToGroups < ActiveRecord::Migration
+  def self.up
+    add_column :groups, :contest,             :boolean
+    add_column :groups, :starts_at,           :datetime
+    add_column :groups, :ends_at,             :datetime
+  end
+
+  def self.down
+    remove_column :groups, :contest
+    remove_column :groups, :starts_at
+    remove_column :groups, :ends_at
+  end
+end
